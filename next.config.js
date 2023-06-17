9629be3f5c9b203b8f68c419a618c9f5975ceae1
Anastasiia Panchenko <nastya.panchenko2306@gmail.com>
2023-06-17 21:37:35 +0300

    envs

--- next.config.js
+++ next.config.js
@@ -4,20 +4,6 @@ const nextConfig = {
   images: {
     domains: ['res.cloudinary.com'],
   },
-  env: {
-    NODE_ENV: process.env.NODE_ENV,
-    MONGODB_URI: process.env.MONGODB_URI,
-    MONGO_USER: process.env.MONGO_USER,
-    MONGO_PASSWORD: process.env.MONGO_PASSWORD,
-    MONGO_DBNAME: process.env.MONGO_DBNAME,
-    NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
-    CLOUDINARY_SECRET: process.env.CLOUDINARY_SECRET,
-    PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID,
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    NEXTAUTH_URL_INTERNAL: process.env.NEXTAUTH_URL_INTERNAL,
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    NEXTAUTH_SITE: process.env.NEXTAUTH_SITE,
-  }
 };
 
 module.exports = nextConfig;
